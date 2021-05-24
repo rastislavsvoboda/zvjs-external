4defb8cd0239d63b70e7ff2667f4f3ed12c8a508
Svoboda Rastislav <svoboda@ditec.sk>
2021-05-24 14:00:23 +0000

    Updated do.bat

--- do.bat
+++ do.bat
@@ -1,7 +1,7 @@
-# test
+## test
 
 git checkout master
 git status
 git remote add upstream https://github.com/rastislavsvoboda/zvjs-external.git
-#git remote add upstream git@github.com:rastislavsvoboda/zvjs-external.git
+## git remote add upstream git@github.com:rastislavsvoboda/zvjs-external.git
 git push -u upstream master
