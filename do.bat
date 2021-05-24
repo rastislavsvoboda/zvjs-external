6fba6cd6d0738aadd261e7f02e72505822ea35d8
Svoboda Rastislav <svoboda@ditec.sk>
2021-05-24 14:07:44 +0000

    using ssh?

--- do.bat
+++ do.bat
@@ -1,7 +1,7 @@
-REM test
+REM test 222
 
 git checkout master
 git status
-git remote add upstream https://github.com/rastislavsvoboda/zvjs-external.git
-REM git remote add upstream git@github.com:rastislavsvoboda/zvjs-external.git
+REM git remote add upstream https://github.com/rastislavsvoboda/zvjs-external.git
+git remote add upstream git@github.com:rastislavsvoboda/zvjs-external.git
 git push -u upstream master
